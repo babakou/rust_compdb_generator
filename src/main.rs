5fdeb0683d6ca52bc6de2c7a0693fcff5253f295
babakou <babakou.1988@gmail.com>
2024-02-05 23:39:21 +0900

    入力ファイルを引数に取れるようにする

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input_file = match args.get(1) {
+        Some(filename) => filename,
+        None => ""
+    };
+
+    if input_file == "" {
+        print_usage();
+        return;
+    }
+
+    println!("input file is {input_file}");
+}
+
+fn print_usage() {
+    println!("usage!");
 }
