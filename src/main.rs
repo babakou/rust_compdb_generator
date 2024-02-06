3da4fd9ac4d4467b9454d2a383a3f5fcc464415d
babakou <babakou.1988@gmail.com>
2024-02-06 21:43:47 +0900

    JSONパースした結果を構造体に格納する処理を追加

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::{env, fs};
 use serde_json::Value;
 
+#[derive(Debug, Default)]
+struct WorkspaceSetting {
+    c_compiler_path: String,
+    cpp_compiler_path: String,
+    root_folder: String,
+    src_pattern: Vec<String>,
+    exclude_pattern: Vec<String>,
+    compile_flags: Vec<String>,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_file = match args.get(1) {
@@ -32,6 +42,34 @@ fn main() {
             return;
         }
     };
+
+    let mut ws_setting = WorkspaceSetting::default();
+    ws_setting.c_compiler_path = json_value["c_compiler_path"].to_string();
+    ws_setting.cpp_compiler_path = json_value["cpp_compiler_path"].to_string();
+    ws_setting.root_folder = json_value["workspace_root_folder"].to_string();
+    ws_setting.src_pattern = 
+        json_value["workspace_src_pattern"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|v| v.to_string())
+        .collect();
+    ws_setting.exclude_pattern = 
+        json_value["workspace_exclude_pattern"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|v| v.to_string())
+        .collect();
+    ws_setting.compile_flags =
+        json_value["workspace_compile_flags"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|v| v.to_string())
+        .collect();
+
+    //println!("{:?}", ws_setting);
 }
 
 fn print_usage() {
